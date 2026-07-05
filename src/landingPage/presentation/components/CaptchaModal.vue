2aec536e0d1ae6eccb107c0bf5af4c1029068274
Usuario353 <u202316353@upc.edu.pe>
2026-07-05 18:40:59 -0500

    Update captcha logo image source path

--- src/landingPage/presentation/components/CaptchaModal.vue
+++ src/landingPage/presentation/components/CaptchaModal.vue
@@ -10,7 +10,7 @@
           </label>
         </div>
         <div class="captcha-right">
-          <img src="/captcha-logo.png" alt="Captcha logo" class="recaptcha-logo" />
+          <img src="/public/captcha-logo.png" alt="Captcha logo" class="recaptcha-logo" />
           <div class="captcha-terms">Privacidad - Condiciones</div>
         </div>
       </div>
